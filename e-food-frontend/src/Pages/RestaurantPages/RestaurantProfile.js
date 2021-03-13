69750736492cc7b60e98cd1b3913173ff80e80e2
Eduardo <eduardo.gheleno@gmail.com>
2021-03-13 16:51:44 -0300

    bug solved: border-radius/restaurant-profile-info

--- e-food-frontend/src/Pages/RestaurantPages/RestaurantProfile.js
+++ e-food-frontend/src/Pages/RestaurantPages/RestaurantProfile.js
@@ -3,10 +3,6 @@ import React from 'react'
 import Nav from '../../Components/Nav'
 import Footer from '../../Components/Footer/Footer'
 
-import StoreIcon from '@material-ui/icons/Store'
-
-// import { red } from '@material-ui/core/colors'
-
 class RestaurantProfile extends React.Component {
     constructor(props) {
         super(props)
@@ -18,16 +14,11 @@ class RestaurantProfile extends React.Component {
 
     render() {
         return (
-            <div className="container-fluid">
+            <div className="container-fluid" style={{ backgroundColor: 'whitesmoke' }}>
                 <Nav profilePage />
-                <div className="restaurant-profile-screen">
+                <div className="restaurant-profile-content">
                     <div className="restaurant-profile-info">
-                        <div className="left-content">
-                            <StoreIcon className="store-icon" style={{ color: 'white', fontSize: 100 }} />
-                        </div>
-                        <div className="right-content">
-                            test
-                        </div>
+                        test
                     </div>
                 </div>
                 <Footer />
